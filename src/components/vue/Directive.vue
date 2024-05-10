88d0e4c5ea03b625413ede932adca78e8b43af4f
jairo ramos <jaramosm@ufpso.edu.co>
2024-05-10 16:30:57 -0500

    avances

---
+++ src/components/vue/Directive.vue
@@ -0,0 +1,24 @@
+<script setup  >
+import { ref } from 'vue';
+
+const ischecked = ref(false)
+</script>
+<template>
+ 
+<div>
+	<input type="checkbox" id="check" v-model="ischecked">
+ <label for="check"> pinchame</label>
+
+</div>
+	 <div v-show="false">
+		este es un mensaje que podria ocultar
+	</div>
+	<div v-if="false">
+		este es un mensaje que podria ocultar
+	</div>
+	<div v-else>
+		esto se ve porque no se cumplio la condicion del if
+	</div> 
+
+ 
+</template>
